f30633540744f8635f38f46a20c4011fc32e7bf9
VelizhaninPavel <velizhaninpavel@gmail.com>
2023-11-11 14:17:24 +0500

    Task7

---
+++ Tyuiu.VelizhaninPI.Sprint4.Task7.V16.Test/DataServiceTest.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using Tyuiu.VelizhaninPI.Sprint4.Task7.V16.Lib;
+
+namespace Tyuiu.VelizhaninPI.Sprint4.Task7.V16.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidCalculate()
+        {
+            DataService ds = new DataService();
+            
+            string str = "382976421897948";
+            int rows = 5;
+            int columns = 3;
+            int wait = 196608;
+            int res = ds.Calculate(rows, columns, str);
+            Assert.AreEqual(wait, res);
+        }
+    }
+}
---
+++ Tyuiu.VelizhaninPI.Sprint4.Task7.V16/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tyuiu.VelizhaninPI.Sprint4.Task7.V16.Test;
+using Tyuiu.VelizhaninPI.Sprint4.Task7.V16.Lib;
+
+namespace Tyuiu.VelizhaninPI.Sprint4.Task7.V16
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+
+            Console.Title = "Спринт #4 | Выполнил: Велижанин П. И. | ИИПб-23-3 ";
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #4                                                               *");
+            Console.WriteLine("* Добавление к решению итоговых проектов по спринту                       *");
+            Console.WriteLine("* Задание #7                                                              *");
+            Console.WriteLine("* Вариант #16                                                             *");
+            Console.WriteLine("* Выполнил: Велижанин Павел Ильич | ИИПб-23-3                             *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Дана строка из одноразрядных цифр '82976421897948'. Преобразуйте ее в   *");
+            Console.WriteLine("* матрицу 5 на 3 и подсчитайте произведение четных чисел.                 *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+            string str = "382976421897948";
+            int rows = 5;
+            int columns = 3;
+            int[,] mtrx = new int[rows, columns];
+            int index = 0;
+
+            Console.WriteLine("\nПолучившийся массив:");
+            for (int i = 0; i < rows; i++)
+            {
+                for (int j = 0; j < columns; j++)
+                {
+                    Console.Write($"{str[index]} \t");
+                    index++;
+                }
+                Console.WriteLine();
+            }
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬАТ:                                                               *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine("Произведение четных чисел массива = " + ds.Calculate(rows, columns, str));
+
+            Console.ReadLine();
+        }
+    }
+}
